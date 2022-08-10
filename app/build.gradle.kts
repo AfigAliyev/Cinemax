825b786c8c2c39b824b4723fdd9b534760f27caf
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2022-08-10 14:32:29 +0400

    Extract SDK versions from build.gradle.kts to libs.versions.toml (#7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,12 +20,12 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.maximillianleonov.cinemax"
-        minSdk = 21
-        targetSdk = 32
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
