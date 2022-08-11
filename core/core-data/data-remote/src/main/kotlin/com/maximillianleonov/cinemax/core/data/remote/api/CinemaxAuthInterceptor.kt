394e52aa7ddd71348c44e98518455279ff222dcf
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2022-08-11 21:26:15 +0400

    Implement CinemaxAuthInterceptor (#21)

---
+++ core/core-data/data-remote/src/main/kotlin/com/maximillianleonov/cinemax/core/data/remote/api/CinemaxAuthInterceptor.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 Maximillian Leonov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.maximillianleonov.cinemax.core.data.remote.api
+
+import com.maximillianleonov.cinemax.core.data.util.Constants
+import okhttp3.Interceptor
+import okhttp3.Response
+
+internal class CinemaxAuthInterceptor(private val apiKeyProvider: CinemaxApiKeyProvider) :
+    Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+
+        val url = request.url.newBuilder()
+            .addQueryParameter(API_KEY_QUERY_PARAM, apiKeyProvider.requireApiKey())
+            .build()
+
+        val newRequest = request.newBuilder().url(url).build()
+        return chain.proceed(newRequest)
+    }
+
+    private companion object {
+        private const val API_KEY_QUERY_PARAM = Constants.Remote.API_KEY_QUERY_PARAM
+    }
+}
