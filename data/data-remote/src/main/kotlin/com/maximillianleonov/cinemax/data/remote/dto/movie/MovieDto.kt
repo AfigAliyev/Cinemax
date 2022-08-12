e23ea7463b720e906c05a4ca9e4288fb8d3868a9
Maximillian Leonov <maximillianleonov@gmail.com>
2022-08-12 19:13:07 +0400

    Add MovieDto

---
+++ data/data-remote/src/main/kotlin/com/maximillianleonov/cinemax/data/remote/dto/movie/MovieDto.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2022 Maximillian Leonov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.maximillianleonov.cinemax.data.remote.dto.movie
+
+import com.maximillianleonov.cinemax.core.data.remote.common.ContentDto
+import com.maximillianleonov.cinemax.core.data.remote.serializer.LocalDateSerializer
+import com.maximillianleonov.cinemax.core.data.util.Constants
+import kotlinx.datetime.LocalDate
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MovieDto(
+    @SerialName(Constants.Fields.ID)
+    override val id: Int,
+
+    @SerialName(Constants.Fields.TITLE)
+    val title: String,
+
+    @SerialName(Constants.Fields.OVERVIEW)
+    val overview: String,
+
+    @SerialName(Constants.Fields.POPULARITY)
+    val popularity: Double,
+
+    @Serializable(LocalDateSerializer::class)
+    @SerialName(Constants.Fields.RELEASE_DATE)
+    val releaseDate: LocalDate,
+
+    @SerialName(Constants.Fields.ADULT)
+    val adult: Boolean,
+
+    @SerialName(Constants.Fields.GENRE_IDS)
+    val genreIds: List<Int>,
+
+    @SerialName(Constants.Fields.ORIGINAL_TITLE)
+    val originalTitle: String,
+
+    @SerialName(Constants.Fields.ORIGINAL_LANGUAGE)
+    val originalLanguage: String,
+
+    @SerialName(Constants.Fields.VOTE_AVERAGE)
+    val voteAverage: Double,
+
+    @SerialName(Constants.Fields.VOTE_COUNT)
+    val voteCount: Int,
+
+    @SerialName(Constants.Fields.POSTER_PATH)
+    val posterPath: String?,
+
+    @SerialName(Constants.Fields.BACKDROP_PATH)
+    val backdropPath: String?,
+
+    @SerialName(Constants.Fields.VIDEO)
+    val video: Boolean
+) : ContentDto
