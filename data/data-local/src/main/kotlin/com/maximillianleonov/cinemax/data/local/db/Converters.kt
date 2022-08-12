5d2dde3e596fc1338980fe6c757aa1801b51627f
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2022-08-12 23:17:25 +0400

    Implement CinemaxDatabase and UpcomingMovie (#60)
    * Add Constants
    
    * Add UpcomingMovieEntity
    
    * Add UpcomingMovieRemoteKeyEntity
    
    * Add UpcomingMovieDao
    
    * Add UpcomingMovieRemoteKeyDao
    
    * Add Converters
    
    * Implement CinemaxDatabase

---
+++ data/data-local/src/main/kotlin/com/maximillianleonov/cinemax/data/local/db/Converters.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2022 Maximillian Leonov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.maximillianleonov.cinemax.data.local.db
+
+import androidx.room.TypeConverter
+import com.maximillianleonov.cinemax.core.data.remote.common.JsonParser
+import com.maximillianleonov.cinemax.core.data.remote.serializer.LocalDateSerializer
+import kotlinx.datetime.LocalDate
+
+object Converters {
+    @TypeConverter
+    fun localDateToJson(value: LocalDate): String = JsonParser.toJson(LocalDateSerializer(), value)
+
+    @TypeConverter
+    fun jsonToLocalDate(json: String): LocalDate = JsonParser.fromJson(LocalDateSerializer(), json)
+
+    @TypeConverter
+    fun intListToJson(value: List<Int>): String = JsonParser.toJson(value)
+
+    @TypeConverter
+    fun jsonToIntList(json: String): List<Int> = JsonParser.fromJson(json)
+}
