3c7ce4e3c8b4737331d8e40acb17c5b05a50db15
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2022-08-15 22:00:37 +0400

    Implement core-presentation components (#84)

---
+++ core/core-presentation/src/main/kotlin/com/maximillianleonov/cinemax/core/presentation/components/Movie.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2022 Maximillian Leonov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.maximillianleonov.cinemax.core.presentation.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.maximillianleonov.cinemax.core.presentation.R
+import com.maximillianleonov.cinemax.core.presentation.theme.CinemaxTheme
+
+@Composable
+fun MoviesContainer(
+    @StringRes titleResourceId: Int,
+    onSeeAllClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(modifier = modifier) {
+        Row(
+            modifier = Modifier
+                .padding(horizontal = CinemaxTheme.spacing.extraMedium)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(id = titleResourceId),
+                style = CinemaxTheme.typography.semiBold.h4,
+                color = CinemaxTheme.colors.textWhite
+            )
+            TextButton(onClick = onSeeAllClick) {
+                Text(
+                    text = stringResource(id = R.string.see_all),
+                    style = CinemaxTheme.typography.medium.h5,
+                    color = CinemaxTheme.colors.primaryBlue
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(CinemaxTheme.spacing.extraSmall))
+        content()
+    }
+}
---
+++ core/core-presentation/src/main/kotlin/com/maximillianleonov/cinemax/core/presentation/components/Placeholder.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 Maximillian Leonov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.maximillianleonov.cinemax.core.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import com.google.accompanist.placeholder.PlaceholderHighlight
+import com.google.accompanist.placeholder.material.placeholder
+import com.google.accompanist.placeholder.material.shimmer
+import com.maximillianleonov.cinemax.core.presentation.theme.CinemaxTheme
+
+@Composable
+fun EmptyPlaceholder(
+    modifier: Modifier = Modifier,
+    visible: Boolean = true,
+    color: Color = CinemaxTheme.colors.primarySoft,
+    shape: Shape = CinemaxTheme.shapes.medium,
+    highlight: PlaceholderHighlight = PlaceholderHighlight.shimmer()
+) = Box(
+    modifier = modifier
+        .fillMaxSize()
+        .placeholder(
+            visible = visible,
+            color = color,
+            shape = shape,
+            highlight = highlight
+        )
+)
