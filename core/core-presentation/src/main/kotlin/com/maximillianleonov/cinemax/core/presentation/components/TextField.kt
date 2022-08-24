256389d04560953aadded019065dc77f2e68ec92
Maximillian Leonov <maximillianleonov@gmail.com>
2022-08-25 00:03:23 +0400

    Implement search text field

---
+++ core/core-presentation/src/main/kotlin/com/maximillianleonov/cinemax/core/presentation/components/TextField.kt
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2022 Maximillian Leonov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.maximillianleonov.cinemax.core.presentation.components
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldColors
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import com.maximillianleonov.cinemax.core.presentation.R
+import com.maximillianleonov.cinemax.core.presentation.theme.CinemaxTheme
+
+@OptIn(ExperimentalMaterialApi::class, ExperimentalAnimationApi::class)
+@Composable
+fun CinemaxTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    @StringRes placeholderResourceId: Int,
+    @DrawableRes iconResourceId: Int,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit) = {
+        Text(text = stringResource(id = placeholderResourceId))
+    },
+    leadingIcon: @Composable (() -> Unit) = {
+        Icon(
+            painter = painterResource(id = iconResourceId),
+            contentDescription = stringResource(id = placeholderResourceId)
+        )
+    },
+    trailingIcon: @Composable (() -> Unit) = {
+        AnimatedVisibility(
+            visible = value.isNotEmpty(),
+            enter = fadeIn() + scaleIn(),
+            exit = scaleOut() + fadeOut()
+        ) {
+            IconButton(onClick = { onValueChange("") }) {
+                Icon(
+                    imageVector = Icons.Rounded.Close,
+                    contentDescription = stringResource(id = R.string.clear)
+                )
+            }
+        }
+    },
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    singleLine: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = CinemaxTheme.shapes.extraMedium,
+    colors: TextFieldColors = TextFieldDefaults.textFieldColors(
+        textColor = CinemaxTheme.colors.textWhite,
+        cursorColor = CinemaxTheme.colors.primaryBlue,
+        backgroundColor = CinemaxTheme.colors.primarySoft,
+        leadingIconColor = CinemaxTheme.colors.textGrey,
+        trailingIconColor = CinemaxTheme.colors.textGrey,
+        placeholderColor = CinemaxTheme.colors.textGrey
+    )
+) {
+    val textColor = textStyle.color.takeOrElse {
+        colors.textColor(enabled).value
+    }
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+
+    @OptIn(ExperimentalMaterialApi::class)
+    BasicTextField(
+        value = value,
+        modifier = modifier.background(colors.backgroundColor(enabled).value, shape),
+        onValueChange = onValueChange,
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = mergedTextStyle,
+        cursorBrush = SolidColor(colors.cursorColor(isError).value),
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        interactionSource = interactionSource,
+        singleLine = singleLine,
+        maxLines = maxLines,
+        decorationBox = @Composable { innerTextField ->
+            TextFieldDefaults.TextFieldDecorationBox(
+                value = value,
+                visualTransformation = visualTransformation,
+                innerTextField = innerTextField,
+                placeholder = placeholder,
+                label = label,
+                leadingIcon = leadingIcon,
+                trailingIcon = trailingIcon,
+                singleLine = singleLine,
+                enabled = enabled,
+                isError = isError,
+                interactionSource = interactionSource,
+                colors = colors,
+                contentPadding = PaddingValues(CinemaxTheme.spacing.default)
+            )
+        }
+    )
+}
--- features/feature-search/presentation/src/main/kotlin/com/maximillianleonov/cinemax/feature/search/presentation/SearchScreen.kt
+++ features/feature-search/presentation/src/main/kotlin/com/maximillianleonov/cinemax/feature/search/presentation/SearchScreen.kt
@@ -16,14 +16,64 @@
 
 package com.maximillianleonov.cinemax.feature.search.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.maximillianleonov.cinemax.core.presentation.R
+import com.maximillianleonov.cinemax.core.presentation.components.CinemaxTextField
+import com.maximillianleonov.cinemax.core.presentation.theme.CinemaxTheme
 
 @Composable
-internal fun SearchRoute(modifier: Modifier = Modifier) {
-    SearchScreen(modifier = modifier)
+internal fun SearchRoute(
+    modifier: Modifier = Modifier,
+    viewModel: SearchViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    SearchScreen(
+        uiState = uiState,
+        onQueryChange = { viewModel.onEvent(SearchEvent.ChangeQuery(it)) },
+        modifier = modifier
+    )
 }
 
-@Suppress("UnusedPrivateMember")
 @Composable
-private fun SearchScreen(modifier: Modifier = Modifier) = Unit
+private fun SearchScreen(
+    uiState: SearchUiState,
+    onQueryChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    focusManager: FocusManager = LocalFocusManager.current
+) {
+    Column(modifier = modifier.fillMaxSize()) {
+        CinemaxTextField(
+            modifier = Modifier
+                .padding(
+                    start = CinemaxTheme.spacing.extraMedium,
+                    top = CinemaxTheme.spacing.small,
+                    end = CinemaxTheme.spacing.extraMedium,
+                    bottom = CinemaxTheme.spacing.extraMedium
+                )
+                .fillMaxWidth(),
+            value = uiState.query,
+            onValueChange = onQueryChange,
+            placeholderResourceId = R.string.search_placeholder,
+            iconResourceId = R.drawable.ic_search,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Sentences,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(onSearch = { focusManager.clearFocus() })
+        )
+    }
+}
