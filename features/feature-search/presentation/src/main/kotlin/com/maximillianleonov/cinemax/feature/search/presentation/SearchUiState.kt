c3267059d9b4ef3a99f4c0f7347ed46e2b82d25c
Maximillian Leonov <maximillianleonov@gmail.com>
2022-08-27 18:40:06 +0400

    Implement Search Movies and TV Shows on search screen

--- features/feature-search/presentation/src/main/kotlin/com/maximillianleonov/cinemax/feature/search/presentation/SearchUiState.kt
+++ features/feature-search/presentation/src/main/kotlin/com/maximillianleonov/cinemax/feature/search/presentation/SearchUiState.kt
@@ -16,15 +16,20 @@
 
 package com.maximillianleonov.cinemax.feature.search.presentation
 
+import androidx.paging.PagingData
 import com.maximillianleonov.cinemax.core.presentation.common.ContentType
 import com.maximillianleonov.cinemax.core.presentation.common.State
 import com.maximillianleonov.cinemax.core.presentation.model.ErrorMessage
 import com.maximillianleonov.cinemax.core.presentation.model.Movie
 import com.maximillianleonov.cinemax.core.presentation.model.TvShow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 data class SearchUiState(
     val query: String = "",
     val isSearching: Boolean = false,
+    val searchMovies: Flow<PagingData<Movie>> = emptyFlow(),
+    val searchTvShows: Flow<PagingData<TvShow>> = emptyFlow(),
     val movies: Map<ContentType.Main, List<Movie>> = emptyMap(),
     val tvShows: Map<ContentType.Main, List<TvShow>> = emptyMap(),
     val error: ErrorMessage? = null
