fe5ea48c718750f3ff7888a043dfc86f7999df4a
Maximillian Leonov <maximillianleonov@gmail.com>
2022-08-30 14:39:48 +0400

    Configure release buildtype

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -64,6 +65,13 @@ android {
             packagingOptions {
                 resources.excludes += "DebugProbesKt.bin"
             }
+
+            @Suppress("ForbiddenComment")
+            // To publish on the Google Play Store a private signing key is required, but to allow
+            // anyone who clones the code to sign and run the release variant, use the debug signing
+            // key.
+            // TODO: Abstract the signing configuration to a separate file to avoid hardcoding this.
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
