71dae46cdf27f93a268b0074b9dd14c36dcd9762
Maximillian Leonov <maximillianleonov@gmail.com>
2022-09-08 15:21:14 +0400

    Add domain models

---
+++ domain/src/main/kotlin/com/maximillianleonov/cinemax/domain/model/MovieDetailsModel.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 Maximillian Leonov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.maximillianleonov.cinemax.domain.model
+
+import kotlinx.datetime.LocalDate
+
+data class MovieDetailsModel(
+    val id: Int,
+    val adult: Boolean,
+    val backdropPath: String?,
+    val budget: Int,
+    val genres: List<GenreModel>,
+    val homepage: String?,
+    val imdbId: String?,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    val posterPath: String?,
+    val releaseDate: LocalDate?,
+    val revenue: Int,
+    val runtime: Int?,
+    val status: String,
+    val tagline: String?,
+    val title: String,
+    val video: Boolean,
+    val voteAverage: Double,
+    val voteCount: Int,
+    val credits: CreditsModel
+)
