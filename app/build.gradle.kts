6c784bbb237043bec3c65bd4781a694dd9de540b
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2022-10-04 23:43:47 +0400

    Update version to 1.0.2 (#189)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
 
     defaultConfig {
         applicationId = "com.maximillianleonov.cinemax"
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 }
 
