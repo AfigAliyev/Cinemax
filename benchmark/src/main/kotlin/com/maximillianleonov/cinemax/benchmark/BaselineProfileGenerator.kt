7e130220b88c66925e436a982ec9ddebb2092792
Afig Aliyev <afigaliyevdev@gmail.com>
2023-07-03 19:54:56 +0400

    Downgrade androidx-benchmark from alpha to stable version

--- benchmark/src/main/kotlin/com/maximillianleonov/cinemax/benchmark/BaselineProfileGenerator.kt
+++ benchmark/src/main/kotlin/com/maximillianleonov/cinemax/benchmark/BaselineProfileGenerator.kt
@@ -16,6 +16,7 @@
 
 package com.maximillianleonov.cinemax.benchmark
 
+import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.maximillianleonov.cinemax.benchmark.util.PackageName
@@ -23,6 +24,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@OptIn(ExperimentalBaselineProfilesApi::class)
 @RunWith(AndroidJUnit4ClassRunner::class)
 class BaselineProfileGenerator {
     @get:Rule
