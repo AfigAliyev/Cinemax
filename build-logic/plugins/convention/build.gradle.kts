f0246ee40851a84202c8545ef8cba8d0a5b75a84
Afig Aliyev <afigaliyevdev@gmail.com>
2023-07-09 13:14:19 +0400

    Upgrade Android Gradle Plugin version to 8.0.2 (#346)

--- build-logic/plugins/convention/build.gradle.kts
+++ build-logic/plugins/convention/build.gradle.kts
@@ -20,11 +20,6 @@ plugins {
 
 group = "com.maximillianleonov.cinemax.buildlogic"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
 dependencies {
     compileOnly(libs.android.gradle.plugin)
     compileOnly(libs.kotlin.gradle.plugin)
